9d4cf99b6aad52ecaad2ec5f61ee98691a5a16c3
Naman2004singh <naman.2singh9@gmail.com>
2026-02-07 11:30:53 +0530

    add CollectionModel class to represent collections with id, title, and product images

---
+++ lib/home/models/collection_model.dart
@@ -0,0 +1,11 @@
+class CollectionModel {
+  final String id;
+  final String title;
+  final List<String> productImages;
+
+  CollectionModel({
+    required this.id,
+    required this.title,
+    required this.productImages,
+  });
+}
