87f9be5a21faa5c421b22cce0d0b9be566b8019a
Naman2004singh <naman.2singh9@gmail.com>
2026-02-07 11:31:05 +0530

    add AppConstants class to define application-wide constants for UI elements

---
+++ lib/utils/constants/app_constants.dart
@@ -0,0 +1,17 @@
+class AppConstants {
+  static const int maxVisibleImages = 5;
+  static const int animationDuration = 300;
+  
+  static const double cardBorderRadius = 16.0;
+  static const double cardElevation = 4.0;
+  static const double cardHorizontalMargin = 8.0;
+  static const double cardVerticalMargin = 8.0;
+  static const double cardPadding = 16.0;
+  
+  static const double imageBorderRadius = 12.0;
+  static const double imageSize = 100.0;
+  static const double imageSpacing = 8.0;
+  
+  static const double titleFontSize = 18.0;
+  static const double overlayFontSize = 24.0;
+}
